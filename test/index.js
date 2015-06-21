37f1e88658d0dd7f4e941c621f16770a859016a4
Benedikt Reiser <benedikt.reiser@innoactive.de>
2015-06-21 16:56:34 +0200

    switched to should as assertion provider

--- test/index.js
+++ test/index.js
@@ -1,8 +1,9 @@
 'use strict';
-var assert = require('assert');
+var should = require('should');
 var gulpScriptsIndex = require('../lib');
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var File = require('vinyl');
 
 describe('gulp-scripts-index', function () {
   // expected array of scripts
@@ -27,17 +28,19 @@ describe('gulp-scripts-index', function () {
    * @param  {Function} done
    */
   var assertWithoutIE = function(files, done) {
-    // loop over the files and create a new array with just the relative paths
-    var relatives = files.map(function(file) {
-      return file.relative;
-    });
-    // now that we have the file list, perform some assertions
-    assert.equal(relatives.length, expected.length, 'there should be ' + expected.length + ' referenced script (without IE scripts) in the input document');
-    // make sure we got all expected scripts
-    for (var i = expected.length - 1; i >= 0; i--) {
-      assert.equal(relatives[i], expected[i], 'should have detected ' + expected[i]);
+    try {
+      // loop over the files and create a new array with just the relative paths
+      var relatives = files.map(function(file) {
+        return file.relative;
+      });
+      // now that we have the file list, perform some assertions
+      relatives.should.have.length(expected.length);
+      // make sure we got all expected scripts
+      relatives.should.containDeepOrdered(expected);
+      done();
+    } catch(e) {
+      done(e);
     }
-    done();
   };
 
   /**
@@ -46,18 +49,19 @@ describe('gulp-scripts-index', function () {
    * @param  {Function} done
    */
   var assertWithIE = function(files, done) {
-    var expected = expectedWithIE;
-    // loop over the files and create a new array with just the relative paths
-    var relatives = files.map(function(file) {
-      return file.relative;
-    });
-    // now that we have the file list, perform some assertions
-    assert.equal(relatives.length, expected.length, 'there should be ' + expected.length + ' referenced script in the input document');
-    // make sure we got all expected scripts
-    for (var i = expected.length - 1; i >= 0; i--) {
-      assert.equal(relatives[i], expected[i], 'should have detected ' + expected[i]);
+    try {
+      // loop over the files and create a new array with just the relative paths
+      var relatives = files.map(function(file) {
+        return file.relative;
+      });
+      // now that we have the file list, perform some assertions
+      relatives.should.have.length(expectedWithIE.length);
+      // make sure we got all expected scripts
+      relatives.should.containDeepOrdered(expectedWithIE);
+      done();
+    } catch(e) {
+      done(e);
     }
-    done();
   };
 
   describe('in-streaming-mode', function () {
@@ -131,19 +135,31 @@ describe('gulp-scripts-index', function () {
       gulp.src('test/input/not-existing.html')
         .pipe(gulpScriptsIndex())
         .pipe(gutil.buffer(function(err, files) {
-          assert.equal(files.length, 0, 'should have detected no files');
+          files.should.have.length(0);
           done();
         }));
     });
 
     it('should detect no script if no empty html file given', function (done) {
-      // try to run plugin with empty html file
-      gulp.src('test/input/empty.html')
-        .pipe(gulpScriptsIndex())
-        .pipe(gutil.buffer(function(err, files) {
-          assert.equal(files.length, 0, 'should have detected no files');
-          done();
+      try {
+        // try to run plugin with empty html file
+        var inputStream = gulp.src('');
+        inputStream .push(new File({
+          path: 'empty.html',
+          contents: null
         }));
+        inputStream
+          .pipe(gutil.buffer(function(err, files) {
+            console.log(files, err);
+          }))
+          .pipe(gulpScriptsIndex())
+          .pipe(gutil.buffer(function(err, files) {
+            files.should.have.length(0);
+            done();
+          }));
+      } catch(e) {
+        done(e);
+      }
     });
   });
 });
